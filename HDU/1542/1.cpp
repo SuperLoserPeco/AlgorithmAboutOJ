9da8a0d915366593bf25048858f1ca555184142f
SPLS <splspeco@SPLSdeiMac.local>
2018-04-12 14:59:35 +0800

    add practice title

---
+++ HDU/1542/1.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>  
+#include <cstring>  
+#include <algorithm>  
+#include <vector>  
+#include <queue>  
+using namespace std;
+
+
+
+int main() {
+	printf("xian fangzaizheli");
+	return 0;
+}
