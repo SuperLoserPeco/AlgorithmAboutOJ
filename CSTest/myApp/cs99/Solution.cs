2fb7c6ad9d3b79a307b9181830dc4c8139097d03
superloserpeco <3323958613@qq.com>
2018-07-12 16:40:15 +0800

    cs99 solution2 commit

--- CSTest/myApp/cs99/Solution.cs
+++ CSTest/myApp/cs99/Solution.cs
@@ -1,3 +1,4 @@
+
 namespace myApp.cs99
 {
 
@@ -9,46 +10,25 @@ public class TreeNode {
 }
 
 public class Solution {
-    TreeNode rt;
     void resolve(TreeNode cur, TreeNode fa) {
-        if(cur.left != null && cur.val < cur.left.val) {
-            TreeNode curleft = cur.left;
-            TreeNode curright = cur.right;
-            cur.left = curleft.left;
-            cur.right = curleft.right;
-            curleft.left = cur;
-            curleft.right = curright;
-            if(fa == null) rt = curleft;
-            else{
-                if(fa.left == cur) fa.left = curleft;
-                else if(fa.right == cur) fa.right = curleft;
-            }
-            cur = curleft;
-        }
-        if(cur.right != null && cur.val > cur.right.val) {
-            TreeNode curleft = cur.left;
-            TreeNode curright = cur.right;
-            cur.left = curright.left;
-            cur.right = curright.right;
-            curright.left = curleft;
-            curright.right = cur;
-            if(fa == null) rt = curright;
-            else {
-                if(fa.left == cur) fa.left = curright;
-                else if(fa.right == cur) fa.right = curright;
-            }
-            cur = curright;
-        }
         if(cur.left != null) resolve(cur.left, cur);
         if(cur.right != null) resolve(cur.right, cur);
+        if(cur.left != null && cur.val < cur.left.val) {
+            int temp = cur.left.val;
+            cur.left.val = cur.val;
+            cur.val = temp;
+        }
+        if(cur.right != null && cur.val > cur.right.val) {
+            int temp = cur.right.val;
+            cur.right.val = cur.val;
+            cur.val = temp;
+        }
     }
 
     public void RecoverTree(TreeNode root) {
-        rt = root;
         if(root != null) {
             resolve(root, null);
         }
-        root = rt;
     }
 }
 }
