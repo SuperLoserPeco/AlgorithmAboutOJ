fcdc6e1cff2dd7cdb19a11699fd86428cb210d32
superloserpeco <3323958613@qq.com>
2018-07-24 17:15:53 +0800

    daily commit

--- LeetCode/18/1.cpp
+++ LeetCode/18/1.cpp
@@ -1,9 +1,32 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        
+        sort(nums.begin(), nums.end());
+        int sz = nums.size();
+
+        vector<vector<int>> ret;
+
+        for(int i = 0; i < sz; i++)
+        for(int j = i + 1; j < sz; j++) {
+            int l1 = j + 1;
+            int l2 = l1 + 1;
+            int cur = nums[i] + nums[j];
+            while(cur + nums[l1] + nums[l2] != k) {
+                if(l1 >= l2) break;
+                if(l1 >= sz || l2 >= sz) break;
+
+                if(cur + nums[l1] + nums[l2] < k) {
+                    if(l2 < sz) l2++;
+                    else l1++;
+                }
+                else if(cur + nums[l1] + nums[l2] == k) {
+                    
+                }
+            }
+        }
     }
 };
