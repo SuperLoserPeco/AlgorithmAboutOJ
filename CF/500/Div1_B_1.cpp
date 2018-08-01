54b91677c038850838e580351f8c527635d807e0
superloserpeco <3323958613@qq.com>
2018-08-01 17:36:13 +0800

    cf not give up

---
+++ CF/500/Div1_B_1.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//答案是对的 但是为什么是这样啊
+
+const int maxn = 4e5 + 100;
+int fa[maxn];
+int weight[maxn];
+
+int find(int x) {
+    if(fa[fa[x]] != fa[x])
+        fa[x] = find(fa[x]);
+    return fa[x];
+}
+
+void unit(int a, int b) {
+    a = find(a);
+    b = find(b);
+
+    if(a == b) return ;
+    if(weight[a] > weight[b]) {
+        weight[a] += weight[b];
+        fa[b] = a;
+    }
+    else {
+        weight[b] += weight[a];
+        fa[a] = b;
+    }
+}
+
+int main() {
+    int n, m, q;
+    cin >> n >> m >> q;
+    for(int i = 1; i <= n + m; i++) {
+        fa[i] = i; weight[i] = i;
+    }
+    while(q--) {
+        int a, b;
+        cin >> a >> b;
+        unit(a, b + n);
+    }
+
+    int ans = -1;
+    for(int i = 1; i <= n + m; i++) {
+        if(fa[i] == i) ans++;
+    }
+
+    cout << ans << endl;
+    return 0;
+}
