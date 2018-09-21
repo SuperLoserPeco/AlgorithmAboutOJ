b56cd36f5f5c887f11126d10a48c15503ee5bd47
SPLS_PECO <mathjohnfh03@163.com>
2018-09-21 17:27:41 +0800

    daily commit

--- LeetCode/146/1.cpp
+++ LeetCode/146/1.cpp
@@ -14,30 +14,48 @@ public:
     int sz;
     map<int, int> mp;
     LRUCache(int capacity) {
-        cout << "capacity" << capacity << endl;
         sz = capacity;
     }
     
     int get(int key) {
+        visit(key);
+        map<int, int>::iterator iter = mp.find(key);
+        if(iter == mp.end()) return -1;
+
+        return mp[key];
+    }
+    
+    void put(int key, int value) {
+        map<int, int>::iterator iter = mp.find(key);
+        if(iter != mp.end()) {
+            visit(key);
+            mp[key] = value;
+            return;
+        }
+        if(Q.size() >= sz) {
+            int lruKey = Q.front();
+            Q.pop_front();
+            deleteKey(lruKey);
+        }
+        Q.push_back(key);
+        mp[key] = value;
+    }
+
+    void visit(int key) {
         for(int i = 0; i < Q.size(); i++) {
             if(Q[i] == key) {
-                cout << *(Q.begin() + i) << endl;
                 Q.erase(Q.begin() + i);
                 Q.push_back(key);
                 break;
             }
         }
-        return mp[key];
     }
-    
-    void put(int key, int value) {
-        if(Q.size() > sz) {
-            int lruKey = Q.front();
-            Q.pop_front();
-            mp[lruKey] = -1;
+
+    void deleteKey(int key) {
+        map<int, int>::iterator iter = mp.find(key);
+        if(iter != mp.end()) {
+            mp.erase(iter);
         }
-        Q.push_back(key);
-        mp[key] = value;
     }
 };
 
@@ -47,3 +65,47 @@ public:
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+/**
+["LRUCache","put","put","get","put","get","put","get","get","get"]
+[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
+
+我的标准输出
+
+capacity2
+1
+2
+3
+4
+
+我的答案
+
+[null,null,null,1,null,2,null,-1,3,4]
+预期答案
+
+[null,null,null,1,null,-1,null,-1,3,4]
+**/
+
+/**
+输入：["LRUCache","get","put","get","put","put","get","get"]
+                    [[2],[2],[2,6],[1],[1,5],[1,2],[1],[2]]
+输出：[null,-1,null,-1,null,null,2,-1]
+预期：[null,-1,null,-1,null,null,2,6]
+
+capacity2
+key before = 2
+
+key after = 2
+
+key before = 1
+2 
+key after = 1
+2 
+key before = 1
+1 1 
+key after = 1
+1 1 
+key before = 2
+1 1 
+key after = 2
+1 1
+**/
---
+++ LeetCode/460/1.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <deque>
+#include <iostream>
+using namespace std;
+
+class LFUCache {
+public:
+    int sz;
+    LFUCache(int capacity) {
+        sz = capacity;
+    }
+    
+    int get(int key) {
+        
+    }
+    
+    void put(int key, int value) {
+        
+    }
+};
